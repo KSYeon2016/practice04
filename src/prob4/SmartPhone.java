1ce338166de6db936b84da708457b22b19880961
S401 <S401@S401-08>
2016-07-12 15:37:15 +0900

    SmartPhone 수정

--- src/prob4/SmartPhone.java
+++ src/prob4/SmartPhone.java
@@ -4,10 +4,7 @@ public class SmartPhone extends MusicPhone {
 
 	@Override
 	public void execute(String function) {
-		if ( function.equals("음악") ) {
-			playMusic();
-			return;
-		} else if(function.equals("앱")){
+		if(function.equals("앱")){
 			playApp();
 			return;
 		}
