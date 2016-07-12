7157eefd03f358ff4b472c5621fca30b2477437b
S401 <S401@S401-08>
2016-07-12 15:34:46 +0900

    Book 수정

--- src/prob3/Book.java
+++ src/prob3/Book.java
@@ -48,17 +48,13 @@ public class Book {
 	// 대여기능을 수행하는 메소드
 	public void rent(){
 		stateCode = 0;
-		System.out.println(getTitle() + "이(가) 대여됐습니다.");
+		System.out.println(title + "이(가) 대여됐습니다.");
 	}
 	
 	// 책의 정보 출력
 	public void print(){
-		System.out.print("책 제목:" + getTitle() + ", 작가:" + getAuthor() + ", 대여 유무:");
-		if(stateCode == 1){
-			System.out.print("재고 있음\n");
-		} else{
-			System.out.print("대여중\n");
-		}
+		System.out.print("책 제목:" + title + ", 작가:" + author
+				+ ", 대여 유무:" + (stateCode == 1? "재고 있음\n" : "대여중\n"));
 	}
 
 }
